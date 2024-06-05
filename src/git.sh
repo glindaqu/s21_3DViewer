5f401a7833db5ddc6f5c25b6d7cc1fcb1c0cdb26
MegaDrage <dragonpsui@gmail.com>
2024-06-05 16:26:00 +0700

    Delete branch checker in sh

--- src/git.sh
+++ src/git.sh
@@ -4,10 +4,4 @@ read -p "Введите название ветки: " branch_name
 
 read -p "Введите origin: " origin
 
-current_branch=$(git rev-parse --abbrev-ref HEAD)
-if [ "$current_branch" != "$branch_name" ]; then
-    echo "Текущая ветка не соответствует введенному имени: $branch_name"
-    exit 1
-fi
-
 git push $origin $branch_name
