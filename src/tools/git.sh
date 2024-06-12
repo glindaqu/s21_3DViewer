accdaa8570595529b4001b2cd5e1f936235fcc66
MegaDrage <dragonpsui@gmail.com>
2024-06-12 21:04:31 +0700

    Git tool changes

--- src/tools/git.sh
+++ src/tools/git.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 usage() {
-    echo "Использование: $0 [-a <файл>] [-r <файл>] [-c <сообщение>] [-p <origin> <ветка>] [-i <сообщение>] [-y <сообщение>] [-h]"
+    echo "Использование: $0 [-a <файл>] [-r <файл>] [-c <сообщение>] [-p <origin> <ветка>] [-i <сообщение>] [-h]"
     echo "  -a <файл>     Добавить файл в индекс Git"
     echo "  -r <файл>     Удалить файл из индекса Git"
     echo "  -c <сообщение>    Создать коммит с сообщением"
-    echo "  -i <сообщение>    Создать коммит с issue"
-    echo "  -y <сообщение>    Создать коммит с yandex tracker"
-    echo "  -p <origin> Отправить изменения в удаленный репозиторий"
+    echo "  -i <сообщение>    Добавить к сообщению issue, yandex_tracker"
+    echo "  -p <origin> <ветка>    Отправить изменения в удаленный репозиторий"
     echo "  -h, --help     Показать эту справку и выйти"
     exit 1
 }
@@ -16,18 +15,17 @@ if [ "$#" -eq 0 ]; then
     git status
 fi
 
-issue_message=""
-yandex_message=""
 add_file=""
 rm_file=""
 push_flag=false
-issue_flag=false
-yandex_flag=false
 commit_message=""
 origin=""
+issue_flag=false
 branch_name=""
+yandex_tracker=""
+issue=""
 
-while getopts ":a:r:c:p:i:y:h:" opt; do
+while getopts ":a:r:c:p:i:h-" opt; do
     case $opt in
         a) add_file="$OPTARG" ;;
         r) rm_file="$OPTARG" ;;
@@ -41,10 +39,9 @@ while getopts ":a:r:c:p:i:y:h:" opt; do
             fi
             ;;
         h) usage ;;
-        i) commit_message="$OPTARG" 
-           issue_flag=true ;;
-        y) commit_message="$OPTARG" 
-           yandex_flag=true ;;
+        i)  
+            issue_flag=true 
+            commit_message="${commit_message}${OPTARG} " ;;
         -)
             case "$OPTARG" in
                 help) usage ;;
@@ -63,6 +60,15 @@ while getopts ":a:r:c:p:i:y:h:" opt; do
     esac
 done
 
+if [ "$push_flag" = true ] && [ -z "$origin" ]; then
+    echo "Ошибка: после опции -p необходимо указать имя удаленного репозитория."
+    usage
+fi
+
+if [ "$push_flag" = true ] && [ -z "$branch_name" ]; then
+    read -p "Branch name: " branch_name
+fi
+
 if [ ! -z "$add_file" ]; then
     git add "$add_file"
 fi
@@ -72,22 +78,19 @@ if [ ! -z "$rm_file" ]; then
 fi
 
 if [ ! -z "$commit_message" ]; then
-    if [ "$issue_flag" = true ] && [ "$yandex_flag" = true ]; then
-        read -p "Issue num: " issue_message
-        read -p "Yandex num: " yandex_message
-        commit_message="#[$issue_message][$yandex_message] $commit_message"
-    elif [ "$issue_flag" = true ]; then
-        read -p "Issue num: " issue_message
-        commit_message="#${issue_message} $commit_message"
-    elif [ "$yandex_flag" = true ]; then
-        read -p "Yandex num: " yandex_message
-        commit_message="$VIEWER-${yandex_message} $commit_message"
+    if [ "$issue_flag" = true ]; then
+        read -p "Issue: " issue
+        read -p "Yandex tracker: " yandex_tracker
+        if [ "$yandex_tracker" -eq 0 ]; then
+            commit_message="#${issue} ${commit_message}"
+        else
+            commit_message="#${issue} ${commit_message}VIEWER-${yandex_tracker}"
+        fi
     fi
     git commit -m "$commit_message"
 fi
 
-if [ $push_flag = true ]; then
-    read -p "Branch name: " branch_name
+if [ "$push_flag" = true ]; then
     if [ ! -z "$origin" ] && [ ! -z "$branch_name" ]; then
         echo "Pushing to $origin $branch_name"
         git push $origin $branch_name
