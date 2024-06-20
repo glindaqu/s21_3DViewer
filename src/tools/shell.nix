473cc8066b366c4ab14fe01d5c727c08f2a588b1
MegaDrage <dragonpsui@gmail.com>
2024-06-20 13:02:55 +0700

    Add nix shell with all packages

---
+++ src/tools/shell.nix
@@ -0,0 +1,17 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "GTK-C-SHELL";
+  buildInputs = with pkgs; [
+    gcc
+    gnumake
+    check
+    pkg-config
+    clang-tools
+    gtk4
+    gtk3
+    glib
+    glibc
+    libepoxy
+    python3
+  ];
+}
